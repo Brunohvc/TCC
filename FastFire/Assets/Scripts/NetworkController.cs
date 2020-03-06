c546580c12e39cd751a75b3ea30e775fbd881177
Brunohvc <bruninho.carvalho159@gmail.com>
2020-03-06 18:58:17 -0300

    project 2019

---
+++ FastFire/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+        // PhotonNetwork.JoinRandomRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FastFire/Assets/Scripts/QuickStart/QuickStartLobbyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject quickStartButton; // Button used for creating and joining a game.
+    [SerializeField]
+    private GameObject quickCancelButton; // Button used for stop searing for a game to join
+    [SerializeField]
+    private int RoomSize; // Manual set the number of players in the room at one time
+    public override void OnConnectedToMaster() // Callback function for when ther first connection is established
+    {
+        PhotonNetwork.AutomaticallySyncScene = true; // Makes it so whatever scene the master client
+        quickStartButton.SetActive(true);
+    }
+
+    public void QuickStart() // Paired to the Quick Star button
+    {
+        quickStartButton.SetActive(false);
+        quickCancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom(); // First tries to join an existing room
+        Debug.Log("Quick start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Creating room now");
+        int randomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)RoomSize };
+        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
+        Debug.Log(randomRoomNumber);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create room... trying again");
+        CreateRoom();
+    }
+
+    public void QuickCancel()
+    {
+        quickCancelButton.SetActive(false);
+        quickStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
