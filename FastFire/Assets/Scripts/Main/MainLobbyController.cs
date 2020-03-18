d32a3493ce072e5c4e0e8fb66c4d730549454add
Brunohvc <bruninho.carvalho159@gmail.com>
2020-03-18 16:48:21 -0300

    start make lobby

---
+++ FastFire/Assets/Scripts/Main/MainLobbyController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class MainLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject lobbyPanel;
+    [SerializeField]
+    private GameObject roomPanel;
+
+    private string roomName;
+    private int roomSize;
+
+    private List<RoomInfo> roomListings;
+    [SerializeField]
+    private Transform roomsContainer;
+    [SerializeField]
+    private GameObject RoomListingPrefab;
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        lobbyPanel.SetActive(true);
+        roomListings = new List<RoomInfo>();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        int tempIndex;
+        foreach(RoomInfo room in roomList)
+        {
+            if (roomListings != null)
+            {
+                tempIndex = roomListings.FindIndex(ByName(room.Name));
+            }
+            else
+            {
+                tempIndex = -1;
+            }
+            if(tempIndex != -1)
+            {
+                roomListings.RemoveAt(tempIndex);
+                Destroy(roomsContainer.GetChild(tempIndex).gameObject);
+            }
+            if(room.PlayerCount > 0)
+            {
+                roomListings.Add(room);
+                ListRoom(room);
+            }
+        }
+    }
+
+    static System.Predicate<RoomInfo> ByName(string name)
+    {
+        return delegate (RoomInfo room)
+        {
+            return room.Name == name;
+        };
+    }
+
+    void ListRoom(RoomInfo room)
+    {
+        if(room.IsOpen && room.IsVisible)
+        {
+            GameObject tempListing = Instantiate(RoomListingPrefab, roomsContainer);
+            RoomButton tempButton = tempListing.GetComponent<RoomButton>();
+            tempButton.SetRoom(room.Name, room.MaxPlayers, room.PlayerCount);
+        }
+    }
+
+    public void OnRoomNameChanged(string nameIn)
+    {
+        roomName = nameIn;
+    }
+
+    public void OnRoomSizeChanged(string sizeIn)
+    {
+        roomSize = int.Parse(sizeIn);
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Creating Room Now!");
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte) roomSize};
+        PhotonNetwork.CreateRoom(roomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Faild Create a Room");
+    }
+
+    public void LogOut()
+    {
+        PhotonNetwork.LeaveLobby();
+        SceneManager.LoadScene(0);
+    }
+}
