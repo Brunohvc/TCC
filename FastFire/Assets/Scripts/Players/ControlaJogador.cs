d0090fac22606d2639cf5a2d4285a270b8076072
Bruno Henrique <brunohvcarvalho@gmail.com>
2020-04-17 21:45:28 -0300

    moving around the map multiplayer

--- FastFire/Assets/Scripts/Players/ControlaJogador.cs
+++ FastFire/Assets/Scripts/Players/ControlaJogador.cs
@@ -5,34 +5,60 @@ using Photon.Pun;
 
 public class ControlaJogador : MonoBehaviourPunCallbacks
 {
+    /*
     public float Velocidade = 10;
     Vector3 direcao;
 
-    public GameObject camera;
-    public GameObject character;
     public Animator animator;
     Vector3 distanciaCompensar;
 
     Actions actions;
+    */
+
+    public GameObject camera;
     PlayerController controller;
+    public GameObject character;
+
+    //Input
+    protected PlayerS Player;
+
+    //Parameters
+    protected const float RotationSpeed = 100;
+
+    //Camera Controll
+    public Vector3 CameraPivot;
+    public float CameraDistance;
+    protected float InputRotationX;
+    protected float InputRotationY;
+
+    protected Vector3 CharacterPivot;
+    protected Vector3 LookDirection;
 
     void Start()
     {
-        // camera.SetActive(photonView.IsMine);
-        // if (!photonView.IsMine) return;
+        camera.SetActive(photonView.IsMine);
+        if (!photonView.IsMine) return;
+        controller = character.GetComponent<PlayerController>();
+        /*
+        camera.SetActive(photonView.IsMine);
+        if (!photonView.IsMine) return;
 
         actions = character.GetComponent<Actions>();
-        controller = character.GetComponent<PlayerController>();
 
         distanciaCompensar = camera.transform.position - transform.position;
-
+        */
         controller.SetArsenal("Rifle");
+        
+
+        Player = FindObjectOfType<PlayerS>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (!photonView.IsMine) return;
+        
+        if (!photonView.IsMine) return;
+        /*
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
@@ -52,14 +78,70 @@ public class ControlaJogador : MonoBehaviourPunCallbacks
             //animator.SetFloat("Blend", 0);
             actions.SendMessage("Stay", SendMessageOptions.DontRequireReceiver);
         }
+        */
+
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //input
+        InputRotationX = InputRotationX + mouseInput.x * RotationSpeed * Time.deltaTime % 360f;
+        InputRotationY = Mathf.Clamp(InputRotationY - mouseInput.y * RotationSpeed * Time.deltaTime, -88f, 88f);
+
+        //left and forward
+        var characterForward = Quaternion.AngleAxis(InputRotationX, Vector3.up) * Vector3.forward;
+        var characterLeft = Quaternion.AngleAxis(InputRotationX + 90, Vector3.up) * Vector3.forward;
+
+        //look and run direction
+        var runDirection = characterForward * (Input.GetAxisRaw("Vertical")) + characterLeft * (Input.GetAxisRaw("Horizontal"));
+        LookDirection = Quaternion.AngleAxis(InputRotationY, characterLeft) * characterForward;
+
+
+        //set player values
+        Player.Input.RunX = runDirection.x;
+        Player.Input.RunZ = runDirection.z;
+        Player.Input.LookX = LookDirection.x;
+        Player.Input.LookZ = LookDirection.z;
+
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Player.Input.SwitchToAK = true;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Player.Input.SwitchToPistol = true;
+        */
+
+        Player.Input.Shoot = Input.GetMouseButton(0);
+
+        RaycastHit hitInfo = new RaycastHit();
+        gameObject.layer = Physics.IgnoreRaycastLayer;
+        int j = Physics.DefaultRaycastLayers;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, PlayerS.AllButIgnoreLayer))
+            Player.Input.ShootTarget = hitInfo.point;
+        else
+            Player.Input.ShootTarget = Vector3.up * 1000f;
+        gameObject.layer = 0;
+
+        // Debug.DrawLine(transform.position + Vector3.up * 1.5f, Player.Input.ShootTarget, Color.magenta);
+
+        CharacterPivot = Quaternion.AngleAxis(InputRotationX, Vector3.up) * CameraPivot;
     }
 
     void FixedUpdate()
     {
+        /*
         // if (!photonView.IsMine) return;
 
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * Velocidade * Time.deltaTime));
 
         camera.transform.position = transform.position + distanciaCompensar;
+        */
+    }
+
+    private void LateUpdate()
+    {
+        if (!photonView.IsMine) return;
+
+        //set camera values
+        camera.transform.position = (transform.position + CharacterPivot) - LookDirection * CameraDistance;
+        camera.transform.rotation = Quaternion.LookRotation(LookDirection, Vector3.up);
     }
 }
---
+++ FastFire/Assets/Scripts/Players/PlayerS.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class PlayerS : MonoBehaviourPunCallbacks
+{
+    [HideInInspector]
+    public InputStr Input;
+    public struct InputStr
+    {
+        public float LookX;
+        public float LookZ;
+        public float RunX;
+        public float RunZ;
+        public bool Jump;
+
+        public bool Shoot;
+        public Vector3 ShootTarget;
+    }
+
+    public float Speed = 10f;
+    public const float JumpForce = 7f;
+
+    protected Rigidbody Rigidbody;
+    protected Quaternion LookRotation;
+
+    public AudioClip AudioClipAK;
+    public AudioClip AudioClipShot;
+
+    protected Animator Animator;
+    protected float Cooldown;
+    protected AudioSource AudioSourcePlayer;
+    protected ParticleSystem ParticleSystem;
+
+    public int HP;
+    public bool IsDead { get { return HP <= 0; } }
+    [HideInInspector]
+    public bool Debug = false;
+    public const int AllButIgnoreLayer = 0b11111011;
+
+    private void Awake()
+    {
+        if (!photonView.IsMine) return;
+
+        HP = 100;
+        Rigidbody = GetComponent<Rigidbody>();
+        Animator = GetComponent<Animator>();
+        AudioSourcePlayer = GetComponent<AudioSource>();
+        ParticleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (!photonView.IsMine) return;
+
+        if (IsDead)
+            return;
+
+        var inputRun = Vector3.ClampMagnitude(new Vector3(Input.RunX, 0, Input.RunZ), 1);
+        var inputLook = Vector3.ClampMagnitude(new Vector3(Input.LookX, 0, Input.LookZ), 1);
+
+        Rigidbody.velocity = new Vector3(inputRun.x * Speed, Rigidbody.velocity.y, inputRun.z * Speed);
+
+        //rotation to go target
+        if (inputLook.magnitude > 0.01f)
+            LookRotation = Quaternion.AngleAxis(Vector3.SignedAngle(Vector3.forward, inputLook, Vector3.up), Vector3.up);
+
+        transform.rotation = LookRotation;
+    }
+
+    void Update()
+    {
+        /*
+        if (!photonView.IsMine) return;
+        if (IsDead)
+            return;
+
+        Cooldown -= Time.deltaTime;
+
+        if (Input.Shoot || Debug)
+        {
+            if (Cooldown <= 0 || Debug)
+            {
+                var shootVariation = UnityEngine.Random.insideUnitSphere;
+
+                Animator.SetTrigger("Shoot");
+                if (Animator.GetBool("AK") == true)
+                {
+                    AudioSourcePlayer.PlayOneShot(AudioClipAK);
+                    Cooldown = 0.2f;
+                    shootVariation *= 0.02f;
+                }
+                else
+                {
+                    AudioSourcePlayer.PlayOneShot(AudioClipShot);
+                    Cooldown = 1f;
+                    shootVariation *= 0.01f;
+                }
+
+                var shootOrigin = transform.position + Vector3.up * 1.5f;
+                var shootDirection = (Input.ShootTarget - shootOrigin).normalized;
+                var shootRay = new Ray(shootOrigin, shootDirection + shootVariation);
+
+
+                //do we hit anybody?
+                var hitInfo = new RaycastHit();
+                gameObject.layer = Physics.IgnoreRaycastLayer;
+                if (Physics.SphereCast(shootRay, 0.1f, out hitInfo, PlayerS.AllButIgnoreLayer))
+                {
+                    UnityEngine.Debug.DrawLine(shootRay.origin, hitInfo.point, Color.red);
+
+                    var player = hitInfo.collider.GetComponent<PlayerS>();
+                    if (player != null && !Debug && player != this)
+                    {
+                        player.OnHit();
+                    }
+                }
+                gameObject.layer = 0;
+
+
+            }
+        }
+
+
+        var charVelo = Quaternion.Inverse(transform.rotation) * Rigidbody.velocity;
+        Animator.SetFloat("SpeedForward", charVelo.z);
+        Animator.SetFloat("SpeedSideward", charVelo.x * Mathf.Sign(charVelo.z + 0.1f));*/
+    }
+
+    private void OnHit()
+    {
+        /*
+        if (!photonView.IsMine) return;
+        if (IsDead)
+            return;
+
+        HP = HP - 10;
+        ParticleSystem.Stop();
+        ParticleSystem.Play();
+        if (IsDead)
+            Die();
+        //Activate Particles*/
+    }
+
+    private void Die()
+    {
+        // Activate Ragdoll Mode
+        // GetComponent<PlayerRagdoll>().RadollSetActive(true);
+    }
+}
