26838ea82b7a9e93abd3c375654d795ff38ae19a
Jeferson Guesser <jefeguesser@gmail.com>
2020-06-05 21:20:36 -0300

    Server connection

--- FastFire/Assets/Scripts/Login/LobbyControllerLogin.cs
+++ FastFire/Assets/Scripts/Login/LobbyControllerLogin.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
-using Photon.Realtime;
 using UnityEngine.SceneManagement;
+using System.Net;
+using System.IO;
+using Assets.Scripts.Login;
 
 public class LobbyControllerLogin : MonoBehaviourPunCallbacks
 {
@@ -57,8 +57,10 @@ public class LobbyControllerLogin : MonoBehaviourPunCallbacks
 
     public void LoginOnClick()
     {
-        if (playerLoginInput.text != "")
+        if (playerLoginInput.text != "" && playerPasswordInput.text != "")
         {
+            // make login
+            LogonServer();
             PlayerPrefs.SetString("Login", playerLoginInput.text);
             PlayerPrefs.SetString("Password", playerPasswordInput.text);
             PhotonNetwork.NickName = playerLoginInput.text;
@@ -66,4 +68,37 @@ public class LobbyControllerLogin : MonoBehaviourPunCallbacks
             PhotonNetwork.JoinLobby();
         }
     }
+
+    public void LogonServer()
+    {
+        var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:3333/api/v1/users/login");
+        httpWebRequest.ContentType = "application/json";
+        httpWebRequest.Method = "POST";
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+        {
+            Login login = new Login();
+            login.email = "zanghelinigab@gmail.com"; // playerLoginInput.text;
+            login.password = "123456"; // playerPasswordInput.text
+
+            string json = JsonUtility.ToJson(login);
+
+            streamWriter.Write(json);
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+        using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+        {
+            var result = streamReader.ReadToEnd();
+            Login loginResult = JsonUtility.FromJson<Login>(result);
+            if (!loginResult.message.Contains("Success"))
+            {
+                throw new System.Exception("Login failed");
+            }
+        }
+    }
+
 }
---
+++ FastFire/Assets/Scripts/Login/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Login
+{
+    [Serializable]
+    public class Login
+    {
+        public string email;
+        public string password;
+        public string message;
+    }
+}
